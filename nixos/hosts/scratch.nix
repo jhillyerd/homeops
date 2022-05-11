7c9efbaa4b88b590af670f7ec8e756dba25d6ee2
James Hillyerd <james@hillyerd.com>
2022-05-10 20:57:30 -0700

    wip: runs

--- nixos/hosts/scratch.nix
+++ nixos/hosts/scratch.nix
@@ -2,6 +2,27 @@
 { pkgs, lib, ... }: {
   imports = [ ../common.nix ];
 
+  services.consul = {
+    enable = true;
+    webUi = true;
+
+    extraConfig = {
+      server = true;
+      bootstrap_expect = 1;
+      client_addr = "0.0.0.0";
+    };
+  };
+
+  environment.systemPackages = [ pkgs.vault ];
+
+  services.vault = {
+    enable = true;
+    storageBackend = "consul";
+    # extraConfig = ''
+    #   api_addr = "http://{{GetPublicIP}}:8200"
+    # '';
+  };
+
   roles.tailscale.enable = lib.mkForce false;
 
   users.users.root.initialPassword = "root";
